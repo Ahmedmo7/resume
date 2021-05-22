a7a61634bfc433f48c4a6ea6935a500da2e0a126
Ahmed Mohamed <amoha367@uwo.ca>
2021-05-22 18:51:52 -0400

    added animations and experience section

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Typed from "react-typed";
-import profile from './profile.png';
-import {Fullpage,Slide,HorizontalSlider} from 'fullpage-react';
-
-
+import td from './tdlogo.png';
+import mp from './mpLogo.jpg';
+import cura from './cura.png';
+import uwo from './uwo.png'
+import Figure from 'react-bootstrap/esm/Figure';
+import {Animated} from 'react-animated-css';
 
 function App() {
 
 
   return (
     //Convert each of the labeled blocks into components as needed
-    <div className="App">
+    <div className="App"
+    style={{
+      // background:"linear-gradient(90deg, rgba(179,136,136,0.8631827731092436) 5%, rgba(237,227,208,0.6895133053221288) 50%, rgba(179,136,136,1) 95%)",
+    }}
+    >
+
+  <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
+  </head>
+
 
     {/** Introduction */}
     <Jumbotron>
@@ -26,7 +37,7 @@ function App() {
         <Col>
         <Typed
             strings={['<h3>Ahmed Mohamed</h3>']}
-            typeSpeed={50}
+            typeSpeed={60}
             showCursor = {false}
             />
             </Col>
@@ -35,7 +46,7 @@ function App() {
       <Col>
             <Typed
             strings={['<h5>4th year Computer Science Student </h5>']}
-            typeSpeed={80}
+            typeSpeed={50}
             startDelay ={30}
             showCursor = {false}
             />
@@ -47,7 +58,7 @@ function App() {
 
     {/** Contact Details */}
     <Container >
-      <h2>Contact Details</h2>
+    <h2>Contact Details</h2>
       <br></br>
       <Row>
         <Col>
@@ -68,7 +79,10 @@ function App() {
         <br></br>
         <br></br>
         <a href="https://www.linkedin.com/in/ahmed-mohamed-b85686173/" target="0"><h5>LinkedIn</h5></a>
+        <br></br>
+        <br></br>
         </Col>
+        
         <Col>
         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
           <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
@@ -78,17 +92,10 @@ function App() {
         <a href="https://github.com/Ahmedmo7" target="0"><h5>GitHub</h5></a>
         </Col>
       </Row>
-      
-      
-      <br></br>
-    </Container>
-   
-   {/**Skills Section */}
-    <Container >
-      <br></br>
-    <h2>Skills</h2>
-    <br></br>
-    <Row>
+
+      {/* <Row>
+      <h2>Skills</h2>
+
       <Col>
       <h5>Programming Languages</h5>
         Python, Java, JavaScript, C, SQL, R ,HTML/CSS
@@ -101,59 +108,195 @@ function App() {
       <h5>Software Tools</h5>
       Git, Unix, Linux, Jira, Confluence, Bootstrap, Azure
       </Col>
-    </Row>
     <br></br>
+
+      </Row> */}
+      
     </Container>
-
-
+   
+<Container>
+  <h2>Education</h2>
+  <br></br>
+  <Animated animationIn="slideInLeft" animationOut="fadeOutUp" animationInDuration={6000} animationOutDuration={800} isVisible={true}>
+        <Row>
+          <Col>
+          <Card>
+            <Card.Header>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={uwo}
+              />
+          </Figure>     
+         <h5>Western University</h5>  
+         <p>May 2018 - June 2022</p>
+         </Card.Header>
+              <Card.Body>
+                <Card.Title>Major in Computer Science</Card.Title>
+                  <Card.Text>
+                  President of Computer Science Undergraduate Society, Junior Varsity Volleyball, Western AI Project Team Member, Intramural Sports
+                   </Card.Text>
+              </Card.Body>
+            </Card>
+        </Col>
+      </Row>
+      </Animated>         
+        <br></br>
+</Container>
 
     {/** Experience*/}
     <Container>
 
-      <h2>Projects</h2>
-        <br></br>
+      <h2>Experience</h2>
+      <br></br>
+        <br></br>       
+        <Animated animationIn="bounceInLeft" animationOut="fadeOutUp" animationInDuration={11000} animationOutDuration={800} isVisible={true}>
         <Row>
-          <Col md={{ span: 3, offset: 3}}>
-            <Card style={{ width: '15rem' }}>
-              <Card.Img variant="top" src={profile} />
+          <Col md={{ span: 7, offset: -5}}>
+          <Card>
+            <Card.Header>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={td}
+              />
+          </Figure>     
+         <h5>TD Bank Group</h5>  
+         <p>May 2021 - August 2021</p>
+         </Card.Header>
               <Card.Body>
-                <Card.Title>COVID</Card.Title>
-                <Card.Text>
-               Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
+                <Card.Title>Business Analyst - IT Specialist Intern</Card.Title>
+                  <Card.Text>
+                  4-month Summer Internship, in the Innovative Automation Corporate Hub
+                   </Card.Text>
+                   <Card.Text>
+                   Maintain and create internal corporate websites using HTML/CSS          
+                   </Card.Text>
+                   <Card.Text>
+                   Used Software Design Specifications to create a canvas app that utilized a CDS database to manage form requests
+                   </Card.Text>
+              </Card.Body>
+            </Card>
+        </Col>
+      </Row>
+      </Animated>         
+        <br></br>
+
+        <Animated animationIn="bounceInRight" animationOut="fadeOutUp" animationInDuration={13000} animationOutDuration={800} isVisible={true}>
+        <Row>
+        <Col md={{ span: 7, offset: 5 }}>
+       <Card>
+            <Card.Header>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={mp}
+              />
+          </Figure>     
+         <h5>MP Web Consulting</h5>
+         <p>August 2020 - present</p>
+         </Card.Header>
+              <Card.Body>
+                <Card.Title>CoFounder and Lead Development Consultant</Card.Title>
+                  <Card.Text>
+                  Co-founded a web development and consulting firm.
+                   </Card.Text>
+                   <Card.Text>
+                   Managed full-stack development of websites and web applications.
+                   </Card.Text>
+                   <Card.Text>
+                   Used Django-bootstrap as the main stack for our projects
+                   </Card.Text>
+                   <Card.Text>
+                   Organize meetings with clients to determine project requirements and specifications
+                   </Card.Text>
               </Card.Body>
             </Card>
         </Col>
         </Row>
-<br></br>
-<br></br>
-<br></br>
-
+        </Animated>
+        <br></br>
+        
+        <Animated animationIn="bounceInLeft" animationOut="fadeOutUp" animationInDuration={15000} animationOutDuration={800} isVisible={true}>
         <Row>
-        <Col md={{ span: 6, offset: 6 }}>
-            <Card style={{ width: '15rem' }}>
-              <Card.Img variant="top" src={profile} />
+          <Col md={{ span: 7, offset: -5}}>
+          <Card>
+            <Card.Header>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={cura}
+              />
+          </Figure>     
+         <h5>Curabit</h5>
+         <p>July 2020 - present</p>
+      </Card.Header>
               <Card.Body>
-                <Card.Title>COVID</Card.Title>
-                <Card.Text>
-               Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
+                <Card.Title>Software Engineer</Card.Title>
+                  <Card.Text>
+                  used ploty.js for data visualization and topic analysis on the client facing dashboard.                   
+                  </Card.Text>
+                   <Card.Text>
+                   wrote a tutorial article on how to use the Flair NPL api to create your first text classifier                 
+                     </Card.Text>
+                   <Card.Text>
+                   researched various NLP machine learning models to better optimize the service.
+                   </Card.Text>
               </Card.Body>
             </Card>
         </Col>
         </Row>
+        <br></br>
+    </Animated>
+        <Animated animationIn="bounceInRight" animationOut="fadeOutUp" animationInDuration={17000} animationOutDuration={800} isVisible={true}>
+
+        <Row>
+        <Col md={{ span: 7, offset: 5 }}>
+        <Card>
+            <Card.Header>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={uwo}
+              />
+          </Figure>     
+         <h5>Western University</h5>
+         <p>September 2019 - April 2020</p>
+         </Card.Header>
+              <Card.Body>
+                <Card.Title>Assistant Web Developer</Card.Title>
+                  <Card.Text>
+                  A member of the Western University Web Team that ensures the accessibility and overall structure of the .uwo.ca sites
+                   </Card.Text>
+                   <Card.Text>
+                   Created a python app that will generate code for an svg based on the UWO Web guidelines using the webbrowser module.
+                   </Card.Text>
+                   <Card.Text>
+                   Used googles dialogue flow to develop a FAQ chatbot that can be embedded into the specific .uwo webpages.
+                   </Card.Text>
+              </Card.Body>
+            </Card>
+        </Col>
+        </Row>
+        </Animated>
 
         <br></br>
         <br></br>
         <br></br>
         <br></br>
         <br></br>
-
 
     </Container>
-
 
     {/* <Container>
       <br></br>
@@ -192,11 +335,7 @@ function App() {
     */}
 
 
-
-
-
     </div>
-
     
   );
 }
